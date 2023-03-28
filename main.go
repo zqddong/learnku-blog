1d20d5adb6f7766e75f403f4098a560142c40ee7
zqddong <zqddong2019@gmail.com>
2023-03-28 17:06:55 +0800

    remove url path more /

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +44,18 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+
+		// 2. 将请求传递下去
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// 重构：使用 gorilla/mux 重写路由
 	router := mux.NewRouter()
@@ -65,7 +78,7 @@ func main() {
 	articleURL, _ := router.Get("articles.show").URL("id", "23")
 	fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe("localhost:3000", router)
+	http.ListenAndServe("localhost:3000", removeTrailingSlash(router))
 
 	// 重构：使用自定义的 ServeMux 有以下缺点
 	// 不支持 URI 路径参数
