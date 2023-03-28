bf9f5b5027a557449a1a8ee925c4db0938420343
zqddong <zqddong2019@gmail.com>
2023-03-29 00:01:08 +0800

    重构 initDB

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/zqddong/learnku-blog/pkg/database"
 	"github.com/zqddong/learnku-blog/pkg/logger"
 	"github.com/zqddong/learnku-blog/pkg/route"
 	"github.com/zqddong/learnku-blog/pkg/types"
-	"strconv"
-	"time"
-
-	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"unicode/utf8"
 )
@@ -21,32 +19,32 @@ import (
 var router *mux.Router
 var db *sql.DB
 
-func initDB() {
-	var err error
-	config := mysql.Config{
-		User:                 "sail",
-		Passwd:               "password",
-		Addr:                 "127.0.0.1:3306",
-		Net:                  "tcp",
-		DBName:               "learnku_blog",
-		AllowNativePasswords: true,
-	}
-
-	// 准备数据库连接池
-	db, err = sql.Open("mysql", config.FormatDSN())
-	logger.LogError(err)
-
-	// 设置最大连接数
-	db.SetMaxOpenConns(25)
-	// 设置最大空闲连接数
-	db.SetMaxIdleConns(25)
-	// 设置每个链接的过期时间
-	db.SetConnMaxLifetime(5 * time.Minute)
-
-	// 尝试连接，失败会报错
-	err = db.Ping()
-	logger.LogError(err)
-}
+//func initDB() {
+//	var err error
+//	config := mysql.Config{
+//		User:                 "sail",
+//		Passwd:               "password",
+//		Addr:                 "127.0.0.1:3306",
+//		Net:                  "tcp",
+//		DBName:               "learnku_blog",
+//		AllowNativePasswords: true,
+//	}
+//
+//	// 准备数据库连接池
+//	db, err = sql.Open("mysql", config.FormatDSN())
+//	logger.LogError(err)
+//
+//	// 设置最大连接数
+//	db.SetMaxOpenConns(25)
+//	// 设置最大空闲连接数
+//	db.SetMaxIdleConns(25)
+//	// 设置每个链接的过期时间
+//	db.SetConnMaxLifetime(5 * time.Minute)
+//
+//	// 尝试连接，失败会报错
+//	err = db.Ping()
+//	logger.LogError(err)
+//}
 
 //func checkError(err error) {
 //	if err != nil {
@@ -476,7 +474,9 @@ func getArticleByID(id string) (Article, error) {
 }
 
 func main() {
-	initDB()
+	//initDB()
+	database.Initialize()
+	db = database.DB
 	//createTables()
 
 	route.Initialize()
