c637d25aa6c321f5adc727a92d8c514265eca688
zqddong <zqddong2019@gmail.com>
2023-03-28 21:58:25 +0800

    保存到数据库

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -29,13 +30,18 @@ func initDB() {
 		AllowNativePasswords: true,
 	}
 
-	db, err := sql.Open("mysql", config.FormatDSN())
+	// 准备数据库连接池
+	db, err = sql.Open("mysql", config.FormatDSN())
 	checkError(err)
 
+	// 设置最大连接数
 	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
 	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
 	db.SetConnMaxLifetime(5 * time.Minute)
 
+	// 尝试连接，失败会报错
 	err = db.Ping()
 	checkError(err)
 }
@@ -98,11 +104,14 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 检查是否有错误
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过!<br>")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
+		lastInsertID, err := saveArticleToDB(title, body)
+		if lastInsertID > 0 {
+			fmt.Fprint(w, "插入成功，ID 为"+strconv.FormatInt(lastInsertID, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
 	} else {
 		storeURL, _ := router.Get("articles.store").URL()
 
@@ -172,6 +181,39 @@ func createTables() {
 ); `
 	_, err := db.Exec(createArticlesSQL)
 	checkError(err)
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+	// 变量初始化
+	var (
+		id   int64
+		err  error
+		rs   sql.Result
+		stmt *sql.Stmt
+	)
+
+	// 1. 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
+	// 例行的错误检测
+	if err != nil {
+		return 0, err
+	}
+
+	// 2. 在此函数运行结束后关闭此语句，防止占用 SQL 连接
+	defer stmt.Close()
+
+	// 3. 执行请求，传参进入绑定的内容
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+
+	// 4. 插入成功的话，会返回自增 ID
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, err
 }
 
 func main() {
