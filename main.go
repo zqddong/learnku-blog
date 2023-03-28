a7df841f7fbe46845c01a03f829acca8a8456cf3
zqddong <zqddong2019@gmail.com>
2023-03-28 16:46:23 +0800

    use serveMux to try

--- main.go
+++ main.go
@@ -19,7 +19,23 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("localhost:3000", nil)
+	// 重构：使用自定义的 ServeMux 有以下缺点
+	// 不支持 URI 路径参数
+	// 不支持请求方法过滤
+	// 不支持路由命名
+	router := http.NewServeMux()
+	router.HandleFunc("/", handlerFunc)
+	router.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe("localhost:3000", router)
+
+	//http.HandleFunc("/", handlerFunc)
+	//http.HandleFunc("/about", aboutHandler)
+	//http.ListenAndServe("localhost:3000", nil)
 }
