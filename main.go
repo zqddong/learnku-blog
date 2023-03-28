19810928c3a99e0024844663c5fcb30126be7741
zqddong <zqddong2019@gmail.com>
2023-03-28 16:55:15 +0800

    集成 Gorilla Mux Router

--- main.go
+++ main.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
+	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,15 +17,55 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "文章 ID："+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "访问文章列表")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "创建新的文章")
+}
+
 func main() {
+	// 重构：使用 gorilla/mux 重写路由
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	// 自定义 404 页面
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 通过命名路由获取 URL 示例
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
+	
+	http.ListenAndServe("localhost:3000", router)
+
 	// 重构：使用自定义的 ServeMux 有以下缺点
 	// 不支持 URI 路径参数
 	// 不支持请求方法过滤
 	// 不支持路由命名
-	router := http.NewServeMux()
-	router.HandleFunc("/", handlerFunc)
-	router.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe("localhost:3000", router)
+	//router := http.NewServeMux()
+	//router.HandleFunc("/", handlerFunc)
+	//router.HandleFunc("/about", aboutHandler)
+	//http.ListenAndServe("localhost:3000", router)
 
 	//http.HandleFunc("/", handlerFunc)
 	//http.HandleFunc("/about", aboutHandler)
