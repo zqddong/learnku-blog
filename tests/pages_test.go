9c4a9e2b00b8ea6eb1fbd4bc53d337be4f8da1a6
zqddong <zqddong2019@gmail.com>
2023-03-28 23:40:22 +0800

    finish code test

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,4 +21,59 @@ func TestHomePage(t *testing.T) {
 	// 2. 检测 —— 是否无错误且 200
 	assert.NoError(t, err, "有错误发生，err 不为空")
 	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
+
+func TestAboutPage(t *testing.T) {
+	baseURL := "http://localhost:3000"
+
+	// 1. 请求 —— 模拟用户访问浏览器
+	var (
+		resp *http.Response
+		err  error
+	)
+	resp, err = http.Get(baseURL + "/about")
+
+	// 2. 检测 —— 是否无错误且 200
+	assert.NoError(t, err, "有错误发生，err 不为空")
+	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
+
+func TestAllPage(t *testing.T) {
+	baseURL := "http://localhost:3000"
+
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/1", 200},
+		{"GET", "/articles/1/edit", 200},
+		{"POST", "/articles/1", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/3/delete", 404},
+	}
+
+	for _, test := range tests {
+		t.Logf("当前请求 URL: %v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+
+		assert.NoError(t, err, "请求 "+test.url+" 时报错")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+" 应返回状态码 "+strconv.Itoa(test.expected))
+	}
 }
