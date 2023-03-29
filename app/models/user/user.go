c6097ec2e238c4a3ff535d172658d7e565557048
zqddong <zqddong2019@gmail.com>
2023-03-29 23:40:55 +0800

    注册表单验证

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -5,7 +5,17 @@ import "github.com/zqddong/learnku-blog/app/models"
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
-	Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-	Password string `gorm:"column:password;type:varchar(255)"`
+	//GORM 默认会将键小写化作为字段名称，column 项可去除，另外默认是允许 NULL 的，故 default:NULL 项也可去除
+	//Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
+	//Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
+	//Password string `gorm:"column:password;type:varchar(255)"`
+	//// gorm:"-" —— 设置 GORM 在读写时略过此字段
+	//PasswordConfirm string `gorm:"-" valid:"password_confirm"`
+
+	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);unique;" valid:"email"`
+	Password string `gorm:"type:varchar(255)" valid:"password"`
+
+	// gorm:"-" —— 设置 GORM 在读写时略过此字段，仅用于表单验证
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
