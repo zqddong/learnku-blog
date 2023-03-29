f0a8132c403ad67ff0a4465f5a8066ecd65b6919
zqddong <zqddong2019@gmail.com>
2023-03-29 18:17:27 +0800

    创建用户

---
+++ app/models/user/crud.go
@@ -0,0 +1,16 @@
+package user
+
+import (
+	"github.com/zqddong/learnku-blog/pkg/logger"
+	"github.com/zqddong/learnku-blog/pkg/model"
+)
+
+// Create 创建用户，通过 User.ID 来判断是否创建成功
+func (user *User) Create() (err error) {
+	if err = model.DB.Create(&user).Error; err != nil {
+		logger.LogError(err)
+		return err
+	}
+
+	return nil
+}
--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"github.com/zqddong/learnku-blog/app/models/articles"
+	"github.com/zqddong/learnku-blog/app/models/user"
 	"github.com/zqddong/learnku-blog/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -20,4 +23,15 @@ func SetupDB() {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	// 创建和维护数据表结构
+	//migration(db)
+}
+
+func migration(db *gorm.DB) {
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&articles.Article{},
+	)
 }
